bdfb1f1aa087e92a099a4f6695890a2988fd8c99
fanxi <397670543@qq.com>
2018-06-28 09:40:44 +0800

    第一次提交

--- app/src/main/java/com/suomate/dabaiserver/activity/configsetting/ConfigAddDeviceActivity.java
+++ app/src/main/java/com/suomate/dabaiserver/activity/configsetting/ConfigAddDeviceActivity.java
@@ -2,16 +2,13 @@ package com.suomate.dabaiserver.activity.configsetting;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import com.suomate.dabaiserver.R;
 import com.suomate.dabaiserver.base.activity.BaseActivity;
-import com.suomate.dabaiserver.utils.config.ContentConfig;
 import com.suomate.dabaiserver.widget.TitleBar;
 
 import butterknife.BindView;
-import butterknife.OnClick;
 
 public class ConfigAddDeviceActivity extends BaseActivity {
 
@@ -38,29 +35,26 @@ public class ConfigAddDeviceActivity extends BaseActivity {
     protected int bindLayout() {
         return R.layout.activity_config_add_device;
     }
-
-    @OnClick({R.id.rl_area, R.id.rl_classification, R.id.rl_niminate, R.id.rl_icon_select,R.id.rl_icon_select})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.rl_area:
-                startActivityForResult(AreaSelectListActivity.class, null, REQUEST_CODE_AREA);
-                break;
-            case R.id.rl_classification:
-                startActivityForResult(ClassifyDeviceListActivity.class, null, REQUEST_CODE_CLASSIFY);
-                break;
-            case R.id.rl_niminate:
-                Bundle bundle=new Bundle();
-                bundle.putInt("type", ContentConfig.TYPE.NOMINATE);
-                startActivityForResult(EditNameActivity.class, bundle, REQUEST_CODE_NOMINATE);
-                break;
-            case R.id.rl_icon_select:
-
-                break;
-
-
-        }
-
-    }
+//
+//    @OnClick({R.id.rl_area, R.id.rl_classification, R.id.rl_niminate, R.id.rl_icon_select, R.id.rl_icon_select})
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.rl_area:
+//                startActivityForResult(AreaSelectListActivity.class, null, REQUEST_CODE_AREA);
+//                break;
+//            case R.id.rl_classification:
+//                startActivityForResult(ClassifyDeviceListActivity.class, null, REQUEST_CODE_CLASSIFY);
+//                break;
+//            case R.id.rl_niminate:
+//                Bundle bundle = new Bundle();
+//                bundle.putInt("type", ContentConfig.TYPE.NOMINATE);
+//                startActivityForResult(EditNameActivity.class, bundle, REQUEST_CODE_NOMINATE);
+//                break;
+//            case R.id.rl_icon_select:
+//                break;
+//        }
+//
+//    }
 
     public void getData() {
         Bundle bundle = getIntent().getExtras();
@@ -75,19 +69,19 @@ public class ConfigAddDeviceActivity extends BaseActivity {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case REQUEST_CODE_AREA:
-                if (RESULT_OK==resultCode) {
+                if (RESULT_OK == resultCode) {
                     data.getStringExtra("areaId");
-                    tvArea.setText( data.getStringExtra("areaName"));
+                    tvArea.setText(data.getStringExtra("areaName"));
                 }
                 break;
             case REQUEST_CODE_CLASSIFY:
-                if (RESULT_OK==resultCode) {
+                if (RESULT_OK == resultCode) {
                     data.getStringExtra("classifyId");
-                    tvClassify.setText( data.getStringExtra("classifyName"));
+                    tvClassify.setText(data.getStringExtra("classifyName"));
                 }
                 break;
             case REQUEST_CODE_NOMINATE:
-                if (RESULT_OK==resultCode) {
+                if (RESULT_OK == resultCode) {
                     tvName.setText(data.getStringExtra("name"));
                 }
                 break;
